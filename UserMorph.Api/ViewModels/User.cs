4e9cb946044c5addb8fd3d68be535312e55afe4d
Aurpan Dash <33072535+Aurpan@users.noreply.github.com>
2023-12-14 15:25:48 +0600

    Feat: add User, UserContact and UserRole view models

---
+++ UserMorph.Api/ViewModels/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UserMorph.Api.ViewModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IsActive { get; set; }
+        public string Company { get; set; }
+        public string Sex { get; set; }
+    }
+}
---
+++ UserMorph.Api/ViewModels/UserContact.cs
@@ -0,0 +1,11 @@
+﻿namespace UserMorph.Api.ViewModels
+{
+    public class UserContact
+    {
+        public int Id { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
