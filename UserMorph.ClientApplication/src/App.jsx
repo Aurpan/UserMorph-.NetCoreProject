a11ef125963cdef7758b46c8b92e64110b59077a
Aurpan Dash <33072535+Aurpan@users.noreply.github.com>
2023-12-18 13:33:18 +0600

    Feat: add bootstarp

--- UserMorph.ClientApplication/src/App.jsx
+++ UserMorph.ClientApplication/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
 
   return (
     <>
-      <div>
-        Gala Day
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            
+          </div>
+        </div>
       </div>
     </>
   )
